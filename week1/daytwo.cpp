e9e4aac3d2c073bfa2c2c7cb443408806c8037fb
Arya <aryaravi211@gmail.com>
2026-04-22 00:07:58 -0400

    Day 2 types,variables,printStars

---
+++ week1/daytwo.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+
+int multiply(int a, int b);
+
+bool isEven(int n);
+
+void printStars(int count);
+
+int main()
+{
+	cout << "multiply :" << multiply(2, 5) << endl;
+
+	cout << boolalpha;
+	cout << "iseven:" << isEven(10) << endl;
+	cout << "iseven: " << isEven(15) << endl;
+
+	printStars(4);
+
+	//cout << printStars(4) << endl;
+
+	return 0;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+bool isEven(int n)
+{
+	if (n % 2 == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void printStars(int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+}
